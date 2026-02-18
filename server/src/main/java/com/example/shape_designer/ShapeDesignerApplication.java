6565ddd8fc89fdc468687cf23f057eff915f21f1
Sahan_SJ <sahansandeepa0003@gmail.com>
2026-02-18 22:48:44 +0530

    refact: fixing some know bugs and adding env for better security

--- server/src/main/java/com/example/shape_designer/ShapeDesignerApplication.java
+++ server/src/main/java/com/example/shape_designer/ShapeDesignerApplication.java
@@ -1,13 +1,19 @@
 package com.example.shape_designer;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShapeDesignerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShapeDesignerApplication.class, args);
-	}
+    public static void main(String[] args) {
+		
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> 
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 
+        SpringApplication.run(ShapeDesignerApplication.class, args);
+    }
 }
